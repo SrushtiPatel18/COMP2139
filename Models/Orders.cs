e3d588b3a5410599b9c36a029928b0e7b38c5d12
SrushtiPatel18 <srushtimpatel007@gmail.com>
2025-04-12 22:33:50 -0400

    Delete Models directory

--- Models/Orders.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment1.Models;
-
-public class Orders
-{
-    public int Id { get; set; }
-    public DateTime OrderDate { get; set; }
-    public decimal TotalPrice { get; set; }
-    public List<OrderItems> OrderItems { get; set; }
-}
